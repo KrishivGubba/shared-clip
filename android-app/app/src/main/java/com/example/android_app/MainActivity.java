673ffc34b710bba20ffd5c77a80e1a5214b5cea4
Krishiv Gubba <kgubba@wisc.edu>
2025-03-18 00:41:04 -0500

    add random key generator to android

--- android-app/app/src/main/java/com/example/android_app/MainActivity.java
+++ android-app/app/src/main/java/com/example/android_app/MainActivity.java
@@ -2,16 +2,39 @@ package com.example.android_app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
+    private static String generateKey(int length){
+        Random rand = new Random();
+        String build = "";
+        for (int i = 0; i < 12; i++){
+            int pick = rand.nextInt();
+            if (pick%2 == 0) {
+                int randomNumber = rand.nextInt(26);
+                char randomChar = (char) ('a' + randomNumber);
+                build = build.concat(String.valueOf(randomChar));
+            } else{
+                int randomNumber = rand.nextInt(9);
+                build = build.concat(String.valueOf(randomNumber));
+            }
+        }
+        return build;
+    }
+
+    public void clickMe(View view){
+        TextView text = findViewById(R.id.enterText);
+        text.setText(generateKey(12));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText usernameInput = findViewById(R.id.prompt);
         System.out.println("hello");
     }
 }
